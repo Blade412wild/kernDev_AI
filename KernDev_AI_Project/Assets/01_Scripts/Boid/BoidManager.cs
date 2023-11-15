cb2e4a9f2dd5df6d169b975a826b274b76965b11
Blade412wild <nathanhoh951@gmail.com>
2023-11-15 13:10:02 +0100

    Calculated the center of the mass of the neigbour group

--- KernDev_AI_Project/Assets/01_Scripts/Boid/BoidManager.cs
+++ KernDev_AI_Project/Assets/01_Scripts/Boid/BoidManager.cs
@@ -14,7 +14,7 @@ public class BoidManager : MonoBehaviour
     [Header("Boids")]
     public boid BoidPrefab;
     public int AmountBoids = 3;
-    public Transform Center;
+    public GameObject CenterPrefab;
     public float NeighboursRadius = 5f;
 
 
@@ -45,12 +45,17 @@ public class BoidManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CalculateAverageMass();
         foreach (boid boid in BoidList)
         {
             List<Transform> neighbours = GetNearbyNeighbours(boid);
+            Vector3 centerOfMass = CalculateAverageMass(neighbours);
+            foreach (Transform _transform in neighbours)
+            {
+                _transform.position = centerOfMass;
+            }
+
         }
-        
+
     }
 
     private Vector3 CalculateRandomSpawn()
@@ -65,23 +70,50 @@ public class BoidManager : MonoBehaviour
 
     private Vector3 CalculateAverageDirection()
     {
-        Vector3 averageDirection = new Vector3(0,0,0);
+        Vector3 averageDirection = new Vector3(0, 0, 0);
 
         return averageDirection;
     }
-    private void CalculateAverageMass()
+    //private void CalculateAverageMass()
+    //{
+    //    Vector3 totalBoidsPos = new Vector3(0, 0, 0);
+    //    for (int i = 0; i < BoidList.Count; i++)
+    //    {
+    //        boid boid = BoidList[i];
+    //        Vector3 BoidPos = boid.transform.position;
+    //        totalBoidsPos = totalBoidsPos + BoidPos;
+    //        //Debug.Log(boid.transform.name + " positie = " + BoidPos);
+    //    }
+
+    //    c = totalBoidsPos / BoidList.Count;
+    //    Center.position = c;
+
+    //    //Debug.Log("CenterOfMass = " + c);
+    //}
+
+    private Vector3 CalculateAverageMass(List<Transform> _neigboursList)
     {
+        Debug.Log(_neigboursList.Count);
         Vector3 totalBoidsPos = new Vector3(0, 0, 0);
-        for (int i = 0; i < BoidList.Count; i++)
+        for (int i = 0; i < _neigboursList.Count; i++)
         {
-            boid boid = BoidList[i];
-            Vector3 BoidPos = boid.transform.position;
-            totalBoidsPos = totalBoidsPos + BoidPos;
+            Transform neiboursTransforms = _neigboursList[i];
+            Vector3 neigbourPos = neiboursTransforms.position;
+            totalBoidsPos = totalBoidsPos + neigbourPos;
             //Debug.Log(boid.transform.name + " positie = " + BoidPos);
         }
 
-        c = totalBoidsPos / BoidList.Count;
-        Center.position = c;
+        
+        Vector3 c = totalBoidsPos / _neigboursList.Count;
+        Debug.Log("the Total vector : " + totalBoidsPos + " | Center of the mass : " + c + "neigbourcount : " + _neigboursList.Count);
+
+        //Debug.Log(c);
+        return c;
+        //GameObject center;
+
+        //center = Instantiate(CenterPrefab, c, Quaternion.identity);
+
+        //center.transform.position = c;
 
         //Debug.Log("CenterOfMass = " + c);
     }
