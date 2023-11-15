3c816de4457d7102b40b667df393d5998b921d9b
Blade412wild <nathanhoh951@gmail.com>
2023-11-15 11:10:46 +0100

    Added find Neigbours

--- KernDev_AI_Project/Assets/01_Scripts/Boid/BoidManager.cs
+++ KernDev_AI_Project/Assets/01_Scripts/Boid/BoidManager.cs
@@ -12,13 +12,13 @@ public class BoidManager : MonoBehaviour
     public float SpawnRange = 30.0f;
 
     [Header("Boids")]
-    public GameObject BoidPrefab;
+    public boid BoidPrefab;
     public int AmountBoids = 3;
     public Transform Center;
-    public float NeighboursRadius = 2.5f;
+    public float NeighboursRadius = 5f;
 
 
-    private List<GameObject> BoidList = new List<GameObject>();
+    private List<boid> BoidList = new List<boid>();
 
     private Vector3 c = new Vector3(0, 0, 0); //CenterOfMass
     private Vector3 totalBoidsPos = new Vector3(0, 0, 0); //CenterOfMass
@@ -32,11 +32,11 @@ public class BoidManager : MonoBehaviour
         for (int i = 1; i <= AmountBoids; i++)
         {
             Vector3 SpawnPos = CalculateRandomSpawn();
-            GameObject Boid = Instantiate(BoidPrefab, SpawnPos, Quaternion.identity);
+            boid Boid = Instantiate(BoidPrefab, SpawnPos, Quaternion.identity);
             BoidList.Add(Boid);
         }
 
-        foreach (GameObject Boid in BoidList)
+        foreach (boid Boid in BoidList)
         {
 
         }
@@ -46,10 +46,11 @@ public class BoidManager : MonoBehaviour
     void Update()
     {
         CalculateAverageMass();
-        foreach (GameObject boid in BoidList)
+        foreach (boid boid in BoidList)
         {
-            List<Transform> neighbours = GetNearbyNeighbours();
+            List<Transform> neighbours = GetNearbyNeighbours(boid);
         }
+        
     }
 
     private Vector3 CalculateRandomSpawn()
@@ -73,33 +74,34 @@ public class BoidManager : MonoBehaviour
         Vector3 totalBoidsPos = new Vector3(0, 0, 0);
         for (int i = 0; i < BoidList.Count; i++)
         {
-            GameObject boid = BoidList[i];
+            boid boid = BoidList[i];
             Vector3 BoidPos = boid.transform.position;
             totalBoidsPos = totalBoidsPos + BoidPos;
-            Debug.Log(boid.transform.name + " positie = " + BoidPos);
+            //Debug.Log(boid.transform.name + " positie = " + BoidPos);
         }
 
         c = totalBoidsPos / BoidList.Count;
         Center.position = c;
 
-        Debug.Log("CenterOfMass = " + c);
+        //Debug.Log("CenterOfMass = " + c);
     }
 
-    //private List<Transform> GetNearbyNeighbours(boid _boid)
-    //{
-    //    List<Transform> neighbours = new List<Transform>();
-    //    Collider[] neighboursColliders= Physics.OverlapSphere(_boid.transform.position, NeighboursRadius);
+    private List<Transform> GetNearbyNeighbours(boid _boid)
+    {
+        List<Transform> neighbours = new List<Transform>();
+        Collider[] neighboursColliders = Physics.OverlapSphere(_boid.transform.position, NeighboursRadius);
 
-    //    foreach (Collider collider in neighboursColliders)
-    //    {
-    //        if (collider != _boid.collider)
-    //        {
-    //            neighbours.Add(collider.transform);
-    //        }
-    //    }
-    //    return neighbours;
+        foreach (Collider collider in neighboursColliders)
+        {
+            if (collider != _boid.collider)
+            {
+                neighbours.Add(collider.transform);
+            }
+        }
+        Debug.Log(neighbours.Count);
+        return neighbours;
 
-    //}
+    }
 
 
 
